ab5e311910ce10f1d8bdf26a0fc28be600881e07
Viktor Shilkov <bourbon.tommy@yandex.ru>
2024-03-20 19:34:26 +0300

    fix: storage package, add separated lines

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film getFilm(int id);
+
     List<Film> getFilms();
+
     Film addFilm(Film film);
+
     void deleteFilm(int id);
+
     Film updateFilm(Film film);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserStorage {
+
     User getUser(int id);
+
     List<User> getUsers();
+
     User addUser(User user);
+
     void deleteUser(int id);
+
     User updateUser(User user);
 }
